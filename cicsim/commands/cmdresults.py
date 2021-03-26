6ac1ce56f0e1c8e369af6ff8eeacd754d4a3fd35
Carsten Wulff <carsten@wulff.no>
2021-03-26 15:04:39 +0100

    Updated cicsim results to add specification

--- cicsim/commands/cmdresults.py
+++ cicsim/commands/cmdresults.py
@@ -34,6 +34,92 @@ import yaml
 import shutil as sh
 import glob
 import pandas as pd
+import numpy as np
+import time
+
+class SpecMinMax:
+
+    def __init__(self,obj = None):
+
+        self.min = None
+        self.max = None
+        self.unit = "V"
+        self.scale = 1
+        self.sources = list()
+
+        if(obj):
+            if("src" in obj):
+                if(type(obj["src"]) is list):
+                    for s in obj["src"]:
+                        self.sources.append(s)
+                else:
+                    self.sources.append(obj["src"])
+            if("min" in obj):
+                self.min = eval(obj["min"])
+
+            if("max" in obj):
+                self.max = eval(obj["max"])
+            if("scale" in obj):
+                self.scale = float(obj["scale"])
+            if("unit" in obj):
+                self.unit = obj["unit"]
+
+    def format(self,ser):
+
+        css = list()
+        for v in ser:
+#            print(v)
+#            print(self.max)
+#               print(self.min)
+            if(v > self.max or v < self.min):
+                css.append('background-color:lightcoral')
+            #elif(np.abs(v-self.max)/self.max > 0.1  or np.abs(v-self.min)/self.min > 0.1):
+            #    css.append('background-color:red')
+            else:
+                css.append('')
+        return css
+
+    def applyScale(self,s):
+
+        return s*self.scale
+
+
+
+
+class Specification(dict):
+
+    def __init__(self,testbench):
+        self.fname = testbench + ".yaml"
+        self.sources = list()
+
+        if(os.path.exists(self.fname)):
+            with open(self.fname,"r") as fi:
+                specobj = yaml.safe_load(fi)
+            if(specobj):
+                for k, v in specobj.items():
+                    if("type"in v):
+                        #Preparing for future types
+                        pass
+                    else:
+                        self[k] = SpecMinMax(v)
+                        for s in self[k].sources:
+                            self.sources.append(s)
+                            self[s] = self[k]
+
+    def format(self,s):
+        if(s.name in self):
+            return self[s.name].format(s)
+        else:
+            return ['' for v in s]
+
+    def scale(self,s):
+        if(s.name in self):
+
+            return self[s.name].applyScale(s)
+        else:
+            return s
+
+
 
 class CmdResults(cs.Command):
     """ Summarize results of TESTBENCH """
@@ -42,14 +128,54 @@ class CmdResults(cs.Command):
         self.testbench = testbench
         super().__init__()
 
+    def summaryToMarkdown(self,df_all):
+        print("# Summary")
+        print("|**Parameter**|**View**|**Min** | **Typ** | **Max**|**Unit**|")
+        print("|:---| :-:| :-:| :-:| :-:| :-:|")
+
+        dfg = df_all.groupby(["type"])
+        for c in df_all.columns:
+            if(c in ["name","type"]):
+                continue
+            for ind,df in dfg:
+                print("|%s | %s|%0.4g | %0.4g | %0.4g |" % (c,ind,df[c].min(),df[c].mean(),df[c].max()))
+
+    def allToMarkdown(self,df_all):
+        print("\n\n# All corners")
+        print(dfg.describe().reset_index().to_markdown(tablefmt="github"))
+        print(df_all.to_markdown(index=False,tablefmt="github"))
+
+    def allToHtml(self,df_all):
+        html = df_all.to_html()
+        text_file = open(f"{self.testbench}.html", "w")
+        text_file.write(html)
+        text_file.close()
+
+    def printFails(self,specs,df):
+        df = df[specs.sources + ["name","type","time"]]
+        df.reset_index(inplace=True)
+        #df = df[not "index"]
+        df = df.apply(specs.scale)
+        st = df.style.apply(specs.format)
+
+        text_file = open(f"results/{self.testbench}.html", "w")
+        text_file.write(self.header)
+        text_file.write(df.describe().style.render())
+        text_file.write(st.hide_index().render())
+        text_file.write(self.footer)
+        text_file.close()
+
+
 
     def run(self):
         files = glob.glob(f"output_{self.testbench}/{self.testbench}_*.csv")
         df_all = pd.DataFrame()
         for f in files:
             df = pd.read_csv(f)
-            name = os.path.basename(f).replace("tran_","").replace(".csv","")
+            name = os.path.basename(f).replace(".*_","").replace(".csv","")
             df["name"] = name
+            (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(f)
+            df["time"] = time.ctime(mtime)
             m = re.search("([A-Z]+[a-z]+)[A-Z]",name)
             if(m):
                 df["type"] = m.group(1)
@@ -62,18 +188,72 @@ class CmdResults(cs.Command):
         #- Print each corner
         df_all.drop(columns=["Unnamed: 0"],inplace=True)
 
-        print("# Summary")
-        print("|**Parameter**|**View**|**Min** | **Typ** | **Max**|**Unit**|")
-        print("|:---| :-:| :-:| :-:| :-:| :-:|")
+        specs = Specification(self.testbench)
 
-        dfg = df_all.groupby(["type"])
-        for c in df_all.columns:
-            if(c in ["name","type"]):
-                continue
-            for ind,df in dfg:
-                print("|%s | %s|%0.2g | %0.2g | %0.2g |" % (c,ind,df[c].min(),df[c].mean(),df[c].max()))
+        if(len(specs) > 0):
+            self.printFails(specs,df_all)
+
+    header = """
+
+<html><head>
+<style>
+
+table {
+  background-color: #EEEEEE;
+  width: 100%;
+  text-align: left;
+  border-collapse: collapse;
+}
+table td, table th {
+  border: 3px solid #FFFFFF;
+  padding: 6px 6px;
+}
+table tbody td {
+  font-size: 13px;
+}
+table tr:nth-child(even) {
+  background: #FCFCFC;
+}
+table thead {
+  background: #1C6EA4;
+  background: -moz-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+  background: -webkit-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+  background: linear-gradient(to bottom, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+  border-bottom: 2px solid #444444;
+}
+table thead th {
+  font-size: 15px;
+  font-weight: bold;
+  color: #FFFFFF;
+  border-left: 2px solid #D0E4F5;
+}
+table thead th:first-child {
+  border-left: none;
+}
+
+table tfoot {
+  font-size: 14px;
+  font-weight: bold;
+  color: #FFFFFF;
+}
+table tfoot td {
+  font-size: 14px;
+}
+table tfoot .links {
+  text-align: right;
+}
+table tfoot .links a{
+  display: inline-block;
+  background: #1C6EA4;
+  color: #FFFFFF;
+  padding: 2px 8px;
+  border-radius: 5px;
+}
 
 
-        print("\n\n# All corners")
-        #print(dfg.describe().reset_index().to_markdown(tablefmt="github"))
-        print(df_all.to_markdown(index=False,tablefmt="github"))
+</style>
+<head>
+<body>
+    """
+    footer = """
+    </body></html>"""
