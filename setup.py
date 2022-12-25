f2f162482dd91c7215d49d9ad6ed69695a2a0a35
Carsten Wulff <carsten@wulff.no>
2022-12-25 13:28:24 +0100

    Forgot to change version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cicsim",
-    version="0.0.2",
+    version="0.1.1",
     author="Carsten Wulff",
     author_email="carsten@wulff.no",
     description="Custom IC Creator Simulation Tools",
