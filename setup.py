328ee872ee69da30af49e12ef806919ad4cf529c
Carsten Wulff <carsten@wulff.no>
2024-11-09 18:20:03 +0100

    Added few more things to fftWithHanning

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cicsim",
-    version="0.1.11",
+    version="0.1.12",
     author="Carsten Wulff",
     author_email="carsten@wulff.no",
     description="Custom IC Creator Simulation Tools",
